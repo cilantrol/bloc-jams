a3dc3cbcb32a3d942b69a04013a4d71e98cf1e95
William Gao <gaowhui@gmail.com>
2017-06-12 16:48:06 -0700

    checkpoint8 done

--- scripts/landing.js
+++ scripts/landing.js
@@ -3,21 +3,21 @@ var animatePoints = function()  {
 
   var revealFirstPoint  = function()  {
     points[0].style.opacity = 1;
-    points[0].style.transform = 'scale(1) translate(0)';
-    points[0].style.msTransform = 'scale(1) translate(0)';
-    points[0].style.WebkiTransform = 'scale(1) translate(0)';
+    points[0].style.transform = 'scaleX(1) translateY(0)';
+    points[0].style.msTransform = 'scaleX(1) translateY(0)';
+    points[0].style.WebkiTransform = 'scaleX(1) translateY(0)';
   };
   var revealSecondPoint  = function()  {
     points[1].style.opacity = 1;
-    points[1].style.transform = 'scale(1) translate(0)';
-    points[1].style.msTransform = 'scale(1) translate(0)';
-    points[1].style.WebkiTransform = 'scale(1) translate(0)';
+    points[1].style.transform = 'scaleX(1) translateY(0)';
+    points[1].style.msTransform = 'scaleX(1) translateY(0)';
+    points[1].style.WebkiTransform = 'scaleX(1) translateY(0)';
   };
   var revealThirdPoint  = function()  {
     points[2].style.opacity = 1;
-    points[2].style.transform = 'scale(1) translate(0)';
-    points[2].style.msTransform = 'scale(1) translate(0)';
-    points[2].style.WebkiTransform = 'scale(1) translate(0)';
+    points[2].style.transform = 'scaleX(1) translateY(0)';
+    points[2].style.msTransform = 'scaleX(1) translateY(0)';
+    points[2].style.WebkiTransform = 'scaleX(1) translateY(0)';
   };
   revealFirstPoint();
   revealSecondPoint();
